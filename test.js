e09c96f6b538a4346c1f34263ea9f1e10790b91e
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2024-11-14 13:12:04 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,48 @@
-const data = [
-  { id: 1, SOC: "Value1", unwantedKey1: "removeMe", unwantedKey2: "removeThisToo" },
-  { id: 2, SOC: "Value2", unwantedKey1: "removeMe", unwantedKey2: "removeThisToo" },
-  { id: 3, SOC: "Value3", unwantedKey1: "removeMe", unwantedKey2: "removeThisToo" }
-];
+const processDataRow = (worksheet, item, startRow, maxRowSpan) => {
+    Object.entries(item).forEach(([key, cellData], columnIndex) => {
+        const columnNumber = columnIndex + 1;
 
-// Keys to remove
-const keysToRemove = ["unwantedKey1", "unwantedKey2"];
+        if (Array.isArray(cellData)) {
+            const rowSpan = cellData.length;
 
-// Transform SOC to array and remove unwanted keys
-const updatedData = data.map(item => {
-  // Create a new object without the unwanted keys
-  const filteredItem = Object.keys(item)
-    .filter(key => !keysToRemove.includes(key)) // Exclude unwanted keys
-    .reduce((acc, key) => {
-      acc[key] = item[key]; // Copy over allowed keys
-      return acc;
-    }, {});
+            if (rowSpan === 1) {
+                // Merge cells vertically for a single value
+                worksheet.mergeCells(
+                    startRow,
+                    columnNumber,
+                    startRow + maxRowSpan - 1,
+                    columnNumber
+                );
+                const cell = worksheet.getCell(startRow, columnNumber);
+                applyCellStyles(cell, cellData[0].value, cellData[0].color);
+            } else {
+                // Add individual cells for multiple values
+                cellData.forEach((data, index) => {
+                    const cell = worksheet.getCell(startRow + index, columnNumber);
+                    applyCellStyles(cell, data.value, data.color);
+                });
 
-  // Update SOC to be an array of objects
-  return {
-    ...filteredItem,
-    SOC: [{ value: item.SOC }]
-  };
-});
+                // Fill remaining rows if rowSpan < maxRowSpan
+                if (rowSpan < maxRowSpan) {
+                    const remainingRows = maxRowSpan - rowSpan;
+                    const lastData = cellData[cellData.length - 1]; // Get last data entry
 
-console.log(updatedData);
+                    for (let i = 0; i < remainingRows; i++) {
+                        const cell = worksheet.getCell(startRow + rowSpan + i, columnNumber);
+                        applyCellStyles(cell, '', lastData.color); // Leave value empty but apply color
+                    }
+                }
+            }
+        } else {
+            // Handle non-array values
+            worksheet.mergeCells(
+                startRow,
+                columnNumber,
+                startRow + maxRowSpan - 1,
+                columnNumber
+            );
+            const cell = worksheet.getCell(startRow, columnNumber);
+            applyCellStyles(cell, cellData, null);
+        }
+    });
+};
