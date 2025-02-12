096a210b5d647e134dc1a37ead98843065526783
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2025-02-11 22:06:47 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,38 @@
-<div style={{ display: "flex", alignItems: "center", position: "relative", width: "100%" }}>
-  {/* Centered Text */}
-  <p style={{ flex: 1, textAlign: "center", margin: 0 }}>{item.value}</p>
+function transformData(inputObj, excludeFields = []) {
+    const transformedObj = {};
 
-  {/* Right-Aligned Triangle */}
-  <div style={{ position: "absolute", top: 0, right: 0 }}>
-    <TriangleCorner color="red" borderColor="black" size={20} />
-  </div>
-</div>
+    Object.keys(inputObj).forEach((key) => {
+        // Skip transformation for excluded fields
+        if (excludeFields.includes(key)) {
+            transformedObj[key] = inputObj[key];
+            return;
+        }
+
+        // Process each field
+        const fieldData = inputObj[key];
+
+        if (Array.isArray(fieldData)) {
+            // If array contains objects with userName
+            if (fieldData.length > 0 && fieldData[0]?.userName) {
+                transformedObj[key] = fieldData.map(item => ({
+                    value: item.userName,
+                    color: "#000000" // Default color, modify as needed
+                }));
+            } 
+            // If it's an array of empty strings
+            else if (fieldData.length === 0 || fieldData.every(i => i === "")) {
+                transformedObj[key] = [{ value: "", color: "#CCCCCC" }]; // Default placeholder object
+            } 
+            // If it's some other array, retain original
+            else {
+                transformedObj[key] = fieldData;
+            }
+        } 
+        // Retain any non-array values as-is
+        else {
+            transformedObj[key] = fieldData;
+        }
+    });
+
+    return transformedObj;
+}
