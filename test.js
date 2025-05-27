6854dea75df37e0b9f54515e183106933df24405
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2025-05-27 12:59:59 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { AgCharts } from 'ag-charts-react';
-
-const HireTerminationScatterChart = ({ hireTermData }) => {
-  const [totalCounts, setTotalCounts] = useState({ hires: 0, terminations: 0 });
-
-  useEffect(() => {
-    if (!hireTermData?.length) {
-      setTotalCounts({ hires: 0, terminations: 0 });
-      return;
-    }
-
-    const totals = hireTermData.reduce(
-      (acc, item) => {
-        acc.hires += item.hires || 0;
-        acc.terminations += item.terminations || 0;
-        return acc;
-      },
-      { hires: 0, terminations: 0 }
-    );
-
-    setTotalCounts(totals);
-  }, [hireTermData]);
-
-  return (
-    <div>
-      <h3>Total Hires: {totalCounts.hires}</h3>
-      <h3>Total Terminations: {totalCounts.terminations}</h3>
-
-      {/* Chart component goes here */}
-      <AgCharts options={{ /* chart config */ }} />
-    </div>
-  );
-};
-
-export default HireTerminationScatterChart;
+SELECT 
+  REGEXP_REPLACE(column_name, '_0([1-9])$', '_\\1') AS normalized_column,
+  reports_to_value
+FROM (
+  SELECT
+    REPORTS_TO_01,
+    REPORTS_TO_02,
+    REPORTS_TO_03,
+    REPORTS_TO_04,
+    REPORTS_TO_05,
+    REPORTS_TO_06,
+    REPORTS_TO_07,
+    REPORTS_TO_08,
+    REPORTS_TO_09,
+    REPORTS_TO_10
+  FROM SNF_CE_OPERATIONS_RW.GLOBAL_HIRES
+) src
+UNPIVOT (
+  reports_to_value FOR column_name IN (
+    REPORTS_TO_01,
+    REPORTS_TO_02,
+    REPORTS_TO_03,
+    REPORTS_TO_04,
+    REPORTS_TO_05,
+    REPORTS_TO_06,
+    REPORTS_TO_07,
+    REPORTS_TO_08,
+    REPORTS_TO_09,
+    REPORTS_TO_10
+  )
+);
