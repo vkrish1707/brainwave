7dc8dd4b656b71be13ef7848c391c158490ad8cd
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2025-06-18 08:42:27 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,5 @@
-function extractUniqueTopNodes(dataArray) {
-  const result = {
-    EBVPTopNode: new Set(),
-    EBVPTopNode2: new Set(),
-    EBVPTopNode3: new Set()
-  };
+const rawHeadCountType = req.query.headCountType || '';
+const headCountTypes = rawHeadCountType.split(',').map(s => s.trim());
 
-  for (const item of dataArray) {
-    if (item.EBVPTopNode) result.EBVPTopNode.add(item.EBVPTopNode);
-    if (item.EBVPTopNode2) result.EBVPTopNode2.add(item.EBVPTopNode2);
-    if (item.EBVPTopNode3) result.EBVPTopNode3.add(item.EBVPTopNode3);
-  }
-
-  return {
-    EBVPTopNode: Array.from(result.EBVPTopNode),
-    EBVPTopNode2: Array.from(result.EBVPTopNode2),
-    EBVPTopNode3: Array.from(result.EBVPTopNode3)
-  };
-}
+const empGroups = headCountTypes.flatMap(hc => hcMap[hc] || []);
+const formattedGroups = empGroups.map(g => `'${g}'`).join(', ');
