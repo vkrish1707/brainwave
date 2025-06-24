f1b80a8853d32331dd7516c56646451b481b1119
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2025-06-24 11:01:55 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,33 +1,3 @@
-const [hcTypeError, setHcTypeError] = useState(false);
-const handleHcTypeChange = (e) => {
-  const hcType = e.target.value;
-  setSelectedHcType(hcType);
-
-  if (hcType.length === 0) {
-    setHcTypeError(true); // show red underline
-  } else {
-    setHcTypeError(false); // clear error
-    fetchData(selectedWeek, hcType);
-  }
-};
-
-<Select
-  value={selectedHcType}
-  multiple
-  error={hcTypeError}
-  onChange={handleHcTypeChange}
-  renderValue={(selected) => selected.join(', ')}
-  input={<OutlinedInput label="HC Type" />}
->
-  {hcTypes.map((name) => (
-    <MenuItem key={name} value={name}>
-      <Checkbox checked={selectedHcType.includes(name)} />
-      <ListItemText primary={name} />
-    </MenuItem>
-  ))}
-</Select>
-{hcTypeError && (
-  <FormHelperText sx={{ color: 'red' }}>
-    At least one HC Type must be selected.
-  </FormHelperText>
-)}
+valueFormatter: (params) => {
+  return typeof params.value === 'number' && params.value === 0 ? '' : params.value;
+}
