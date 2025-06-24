708632c5f8bf759b4a80479f229cc4d113f1fb27
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2025-06-24 11:11:36 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-valueFormatter: (params) => {
-  return typeof params.value === 'number' && params.value === 0 ? '' : params.value;
-}
+previousQuarter: (() => {
+    const i = quarters.indexOf(currentQuarter);
+    if (i > 0) {
+        return `${quarters[i - 1]}_${year}`;
+    } else {
+        return `Q4_${year - 1}`;
+    }
+})(),
+nextQuarter: (() => {
+    const i = quarters.indexOf(currentQuarter);
+    if (i < 3) {
+        return `${quarters[i + 1]}_${year}`;
+    } else {
+        return `Q1_${year + 1}`;
+    }
+})(),
