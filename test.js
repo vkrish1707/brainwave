d815c723d1a05c0b1bf481cf874a88b8a68d37ac
Vamsi Krishna Mylavarapu <32238284+vkrish1707@users.noreply.github.com>
2025-08-07 11:44:04 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,103 +1,118 @@
-const BlocklistList = ({ blocklists }) => {
-  return (
-    <div className="blocklist-list">
-      {blocklists.map((b) => {
-        return (
-          <div className="blocklist-list__item" key={b.id}>
-            <div>
-              <div className="blocklist-list__name">{b.name}</div>
-              <div className="blocklist-list__ips">{b.ips.join(', ')}</div>
-            </div>
-            <button className="btn btn--danger blocklist-list__delete-btn">
-              Delete
-            </button>
-          </div>
-        )
-      })}
-    </div>
-  );
-}
+ Blocklist Management Application
+    ✔ should load the page for simple test (1337ms)
+    ✔ should find elements simple test (59ms)
+    ✔ should display ips comma separted in blocklist list (84ms)
+    ✔ should not submit empty form (132ms)
+    1) should add 2 blocklists
+    2) should delete "First Blocklist"
+    3) should add "My Blocklist 3" and delete "My Blocklist 2"
+    4) should delete all blocklists
+    5) should add 4 quotes with duplicated names
+    6) should delete second "My Blocklist"
+    7) should delete last blocklist
 
-const App = () => {
-  const [blockLists, setBlockLists] = React.useState([
-    {
-      id: 1,
-      name: "First Blocklist",
-      ips: ["1.1.1.1", "2.2.2.2", "3.3.3.3"]
-    }
-  ]);
-  
-  const nameRef = React.useRef(null)
-  const ipsRef = React.useRef(null)
-  
-  const handleAddBlockList = (e) => {
-    e.preventDefault();
-    const name = nameRef.current.value.trim();
-    const ips = ipsRef.current.value.split(',').map(ip => ip.trim()).filter(ip => ip);
-    
-    if(!name || ips.length == 0) return;
-    
-    const newBlockList = {
-      id: Date.now(),
-      name,
-      ips
-    }
-    
-    setBlockLists([...blockLists, newBlockList]);
-    nameRef.current.value = "";
-    ipsRef.current.value = "";
-  };
 
-  const handleDeleteBlockList = () => {
-    setBlockLists(blockLists.filter((_, index) => index != indexToDelete));
-  };
+  4 passing (4s)
+  7 failing
 
-  return (
-    <div className="content-container">
-      <h2>Add new ip blocklist</h2>
-      <form className="add-blocklist-form" onSubmit={handleAddBlockList}>
-        <div className="form-field">
-          <label className="form-field__label">Blocklist Name</label>
-          <input
-            id="name-input"
-            className="form-field__input"
-            placeholder="Blocklist Name..."
-            name="name"
-            type="text"
-            ref={nameRef}
-          />
-        </div>
-        <div className="form-field">
-          <label className="form-field__label">Ips</label>
-          <textarea 
-            id="ips-input"
-            placeholder="2.2.2.2, 3.3.3.3, 4.2.2.2"
-            name="ips"
-            className="form-field__input"
-            ref={ipsRef}
-          />
-          <p id="ips-input-helpertext" className="form-field__helper-text">
-            Comma-separated list of ips to add to the blocklist.
-          </p>
-        </div>
-        
-        <div>
-          <button 
-            id="blocklist-submit" 
-            className="btn btn--primary"
-          >
-            Add blocklist
-          </button>
-        </div>
-      </form>
+  1) Blocklist Management Application
+       should add 2 blocklists:
 
-      <h2>Your blocklists</h2>
-      <BlocklistList blocklists={blockLists} onDelete= {handleDeleteBlockList}/>
-    </div>
-  );
-};
+      AssertionError: expected [ Array(1) ] to deeply equal [ Array(3) ]
+      + expected - actual
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+         {
+           "ips": "1.1.1.1, 2.2.2.2, 3.3.3.3"
+           "name": "First Blocklist"
+         }
+      +  {
+      +    "ips": "1.2.3.4, 5.6.7.8"
+      +    "name": "My Blocklist 1"
+      +  }
+      +  {
+      +    "ips": "2.3.4.5, 6.7.8.9"
+      +    "name": "My Blocklist 2"
+      +  }
+       ]
+      
+      at expectBlocklistsToBe (specs/simple.test.js:173:37)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
+      at async Context.<anonymous> (specs/simple.test.js:54:5)
+
+  2) Blocklist Management Application
+       should delete "First Blocklist":
+
+      AssertionError: expected [ Array(1) ] to deeply equal [ Array(2) ]
+      + expected - actual
+
+       [
+         {
+      -    "ips": "1.1.1.1, 2.2.2.2, 3.3.3.3"
+      -    "name": "First Blocklist"
+      +    "ips": "1.2.3.4, 5.6.7.8"
+      +    "name": "My Blocklist 1"
+         }
+      +  {
+      +    "ips": "2.3.4.5, 6.7.8.9"
+      +    "name": "My Blocklist 2"
+      +  }
+       ]
+      
+      at expectBlocklistsToBe (specs/simple.test.js:173:37)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
+      at async Context.<anonymous> (specs/simple.test.js:65:5)
+
+  3) Blocklist Management Application
+       should add "My Blocklist 3" and delete "My Blocklist 2":
+     TypeError: Cannot read properties of undefined (reading 'click')
+      at Context.<anonymous> (specs/simple.test.js:79:36)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
+
+  4) Blocklist Management Application
+       should delete all blocklists:
+     TypeError: Cannot read properties of undefined (reading 'click')
+      at Context.<anonymous> (specs/simple.test.js:91:37)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
+
+  5) Blocklist Management Application
+       should add 4 quotes with duplicated names:
+
+      AssertionError: expected [ Array(1) ] to deeply equal [ Array(4) ]
+      + expected - actual
+
+       [
+         {
+      -    "ips": "1.1.1.1, 2.2.2.2, 3.3.3.3"
+      -    "name": "First Blocklist"
+      +    "ips": "4.5.6.7, 8.9.0.1, 1.1.1.1"
+      +    "name": "My Blocklist"
+         }
+      +  {
+      +    "ips": "4.5.6.7, 8.9.0.1, 2.1.1.1"
+      +    "name": "My Blocklist"
+      +  }
+      +  {
+      +    "ips": "4.5.6.7, 8.9.0.1, 3.1.1.1"
+      +    "name": "My Blocklist"
+      +  }
+      +  {
+      +    "ips": "4.5.6.7, 8.9.0.1, 4.1.1.1"
+      +    "name": "My Blocklist"
+      +  }
+       ]
+      
+      at expectBlocklistsToBe (specs/simple.test.js:173:37)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
+      at async Context.<anonymous> (specs/simple.test.js:117:5)
+
+  6) Blocklist Management Application
+       should delete second "My Blocklist":
+     TypeError: Cannot read properties of undefined (reading 'click')
+      at Context.<anonymous> (specs/simple.test.js:130:37)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
+
+  7) Blocklist Management Application
+       should delete last blocklist:
+     TypeError: Cannot read properties of undefined (reading 'click')
+      at Context.<anonymous> (specs/simple.test.js:144:37)
+      at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
