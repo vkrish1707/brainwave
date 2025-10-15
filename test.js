66eaaa52cf0f910d40daee9e7f3f77486e1449d9
Bhanu <32238284+vkrish1707@users.noreply.github.com>
2025-10-14 22:50:26 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
+# Set NVM_DIR
 setenv NVM_DIR "$HOME/.nvm"
-if (-e "$NVM_DIR/nvm.sh") then
-  source "$NVM_DIR/nvm.sh"
+
+# Load NVM
+if ( -f "$NVM_DIR/nvm.sh" ) then
+    source "$NVM_DIR/nvm.sh"
 endif
-if (-e "$NVM_DIR/bash_completion") then
-  source "$NVM_DIR/bash_completion"
+
+# Load NVM bash_completion if available
+if ( -f "$NVM_DIR/bash_completion" ) then
+    source "$NVM_DIR/bash_completion"
 endif
